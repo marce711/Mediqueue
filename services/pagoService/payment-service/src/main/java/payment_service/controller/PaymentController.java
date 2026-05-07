bc54611235c8eae1d39c8373a234554de4b6596c
DannyVH24 <dannyestuardo321@gmail.com>
2026-05-07 00:08:24 -0600

    Microservicio Pagos - Logs

--- services/pagoService/payment-service/src/main/java/payment_service/controller/PaymentController.java
+++ services/pagoService/payment-service/src/main/java/payment_service/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package payment_service.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import payment_service.model.Payment;
@@ -10,11 +12,26 @@ import payment_service.service.PaymentService;
 @RequestMapping("/payments")
 public class PaymentController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
     @Autowired
     private PaymentService paymentService;
 
     @PostMapping
     public Payment processPayment(@Valid @RequestBody Payment payment) {
-        return paymentService.processPayment(payment);
+
+        logger.info("Solicitud recibida para procesar pago: {}", payment);
+
+        try {
+            Payment result = paymentService.processPayment(payment);
+
+            logger.info("Pago procesado exitosamente. ID o referencia: {}", result);
+
+            return result;
+
+        } catch (Exception e) {
+            logger.error("Error al procesar el pago: {}", payment, e);
+            throw e;
+        }
     }
 }
